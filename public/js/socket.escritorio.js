7dd2d6c5538e0cecbfe86f3c4241af9ccbe2057e
AMUCF001 <Francisco.X.Amuchastegui.-ND@Disney.com>
2020-01-30 12:11:06 -0800

    agrego if en caso q no haya tickets mostrar una alerta

--- public/js/socket.escritorio.js
+++ public/js/socket.escritorio.js
@@ -13,6 +13,11 @@ let label = $("small");
 $("h1").text("Escritorio: " + escritorio);
 $("button").on("click", function() {
   socket.emit("atenderTicket", { escritorio: escritorio }, function(resp) {
-    console.log(resp);
+    if (resp === "No hay tickets") {
+      label.text(resp);
+      alert(resp);
+      return;
+    }
+    label.text("Ticket: " + resp.numero);
   });
 });
